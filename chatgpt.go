2e6339a6367ccf5422872863f0b971380d8cda18
ziyi xi <ziyixi@mail.ustc.edu.cn>
2023-03-27 13:26:48 -0400

    Update prompt

--- chatgpt.go
+++ chatgpt.go
@@ -17,7 +17,7 @@ func summaryByChatGPT(content string) string {
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role: openai.ChatMessageRoleUser,
-					Content: `Summarize the following email in a way that highlights the main idea using bullet points in Markdown format so I can easily recall it later, it can use sub-bullet points to be more organized, and it shouldn't include level 1 Headings:			
+					Content: `I want you to act as a personal email assistant. I will give you the content of an email, and you will provide a summary of that topic in the format of markdown bullet points (very important). Your summary should be concise, covering the most important aspect of the topic. Start your summary with an introductory paragraph that gives an overview of the topic, then bullet points and sub bullet points to make it more organized. It shouldn't include any level of Headings. Below is the content of the email:			
 
 					` + content,
 				},
